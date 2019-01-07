2dd5a89ddc574c3c032c36827b1635590f007fc3
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-01-07 22:09:33 +0100

    eighth test

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,21 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+
+
+
+function average(average){
+     var filtered = average.filter(function(x){
+        return typeof x !== "string";
+    })
+    
+    var sum = 0;
+    for(var i = 0; i < filtered.length; i++){
+        sum += filtered[i];
+    } 
+    var avg = sum / filtered.length;
+    return avg;
+}
+
+module.exports = average;
